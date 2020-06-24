ecfe29453f90546c98cca11f7a4892adcb0e2d2d
AnthonyTC89 <ptonyptc@gmail.com>
2020-06-23 23:15:12 -0500

    Updates

---
+++ precache-manifest.ac30480b472d75dd00789c44f78a0ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03050f6e250be2ce517efb910c062b76",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "a2b230f82fb293740935",
+    "url": "/portfolio-v2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5606011862affcf1568",
+    "url": "/portfolio-v2/static/js/2.b07de7b6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio-v2/static/js/2.b07de7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b230f82fb293740935",
+    "url": "/portfolio-v2/static/js/main.c2c7ef19.chunk.js"
+  },
+  {
+    "revision": "58daaf2044f28a9eccfa",
+    "url": "/portfolio-v2/static/js/runtime-main.a8d6ddc0.js"
+  }
+]);
